bb1ee53fe90fccfe79e56111d4d408bfb94c56da
IdkWhatToWriteHereOhManItWasAlreadyUsed <dimoon.amid05@gmail.com>
2026-02-03 16:25:48 +0300

    Remove OpenGLSomethingFrameDisplayerEVO folder

--- OpenGLSomethingFrameDisplayerEVO/GlobalThreadPool.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <ThreadPool/ThreadPool.h>
-#include <thread>
-
-static ThreadPool globalThreadPool(8);
--- OpenGLSomethingFrameDisplayerEVO/GlobalVectorPool.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "VectorPool/VectorPool.h"
-#include "renderer/Mesh.h"
-
-static VectorPool<Vertex> vertexVectorPool(4444);
-static VectorPool<unsigned int> indexVectorPool(6666);
